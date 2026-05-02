0cc9b804e4a436d23fbaa71a3c446ee80cd4cf92
Mohamed Darwish <mohameddarwesh60@gmail.com>
2026-05-03 00:52:31 +0300

    Extract statistics into Statistics component
    Create a new Statistics.Jsx component and move the statistics UI out of App.jsx. App now imports Statistics and passes good, neutral and bad as props; the new component renders counts and calculates totals, average and positive percentage.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Statistics from "./Statistics.Jsx";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -17,15 +18,7 @@ const App = () => {
         <button onClick={() => setBad((prev) => prev + 1)}>bad</button>
       </div>
       {/* statistics */}
-      <div className="">
-        <h3>statistics</h3>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad} </p>
-        <p>average {all > 0 &&(good - bad)/ all }</p>
-        <p>positive {all > 0 && (good / all) * 100}% </p>
-      </div>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
